48838f590ae7710a6b3e213171b61c341325b536
Raul Oviedo <antoniorah@gmail.com>
2017-03-22 01:27:12 -0400

    Added more account action tests

--- test/actions-test.js
+++ test/actions-test.js
@@ -16,3 +16,29 @@ describe('login actions', () => {
         expect(updatedCredentials.username).to.equal(expectedAction.username);
     });
 });
+
+describe('account actions', () => {
+    it('creates an action to update deposit state', () => {
+        const deposit = {deposit: 200};
+        const expectedAction = {
+            type: 'UPDATE_DEPOSIT',
+            deposit
+        };
+
+        const updatedDeposit = accountActions.updateDeposit(deposit);
+        expect(updatedDeposit.type).to.equal(expectedAction.type);
+        expect(updatedDeposit.deposit).to.equal(expectedAction.deposit);
+    });
+
+    it('creates an action to update withdrawal state', () => {
+        const withdrawal = {withdrawal: 200};
+        const expectedAction = {
+            type: 'UPDATE_WITHDRAWAL',
+            withdrawal
+        };
+
+        const updatedWithdrawal = accountActions.updateWithdrawal(withdrawal);
+        expect(updatedWithdrawal.type).to.equal(expectedAction.type);
+        expect(updatedWithdrawal.withdrawal).to.equal(expectedAction.withdrawal);
+    });
+});
